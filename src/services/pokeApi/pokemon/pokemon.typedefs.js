ebca61d0f3b8abbf2b2de1f10008df33cf003370
Eward Somers <eward@pixelvikings.be>
2021-07-30 09:15:39 +0200

    property auth example

--- src/services/pokeApi/pokemon/pokemon.typedefs.js
+++ src/services/pokeApi/pokemon/pokemon.typedefs.js
@@ -2,7 +2,7 @@ const { gql } = require("apollo-server");
 // The GraphQL schema
 const schema = gql`
   type pokemon {
-    url: String!
+    url: String! @auth(requires: [ADMIN])
     name: String!
   }
 
