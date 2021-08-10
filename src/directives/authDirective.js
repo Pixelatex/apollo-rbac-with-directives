88f7872e72c4666981562216663a0eeefaa5aa92
Eward Somers <eward@pixelvikings.be>
2021-08-10 11:09:23 +0200

    cleanup old code

--- src/directives/authDirective.js
+++ src/directives/authDirective.js
@@ -31,7 +31,6 @@ function authDirective() {
           if (authDirective) {
             const { requires } = authDirective;
             if (requires) {
-              console.log(requires, "???");
               const { resolve = defaultFieldResolver } = fieldConfig;
               fieldConfig.resolve = function (source, args, context, info) {
                 const { user } = context;
@@ -53,51 +52,3 @@ function authDirective() {
   };
 }
 module.exports = authDirective;
-// class AuthDirective extends SchemaDirectiveVisitor {
-//   visitObject(type) {
-//     this.validate(type);
-//     type._requiredAuthRole = this.args.requires;
-//   }
-//   // Visitor methods for nested types like fields and arguments
-//   // also receive a details object that provides information about
-//   // the parent and grandparent types.
-//   visitFieldDefinition(field, details) {
-//     this.validate(details.objectType);
-//     field._requiredAuthRoles = this.args.requires;
-//   }
-
-//   validate(objectType) {
-//     // Mark the GraphQLObjectType object to avoid re-wrapping:
-//     if (objectType._authFieldsWrapped) return;
-//     objectType._authFieldsWrapped = true;
-
-//     const fields = objectType.getFields();
-//     Object.keys(fields).forEach((fieldName) => {
-//       const field = fields[fieldName];
-//       const { resolve = defaultFieldResolver } = field;
-//       field.resolve = async function (...args) {
-//         // Get the required Role from the field first, falling back
-//         // to the objectType if no Role is required by the field:
-//         const requiredRoles =
-//           field._requiredAuthRoles || objectType._requiredAuthRoles;
-//         if (!requiredRoles) {
-//           return resolve.apply(this, args);
-//         }
-
-//         const context = args[2];
-//         const { user } = context;
-//         if (user) {
-//           if (requiredRoles.includes(user.role)) {
-//             return resolve.apply(this, args);
-//           } else {
-//             throw new Error("user lacks permission for this action");
-//           }
-//         } else {
-//           throw new Error("no user found");
-//         }
-//       };
-//     });
-//   }
-// }
-
-// module.exports = AuthDirective;
